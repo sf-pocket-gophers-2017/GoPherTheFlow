6feb6053e025f2c412121a6f42906fb1c2876631
Filmon <filmon.mengisteab@yahoo.com>
2017-08-18 15:51:44 -0700

    Add index controller, errors, nav bar and logout partials, and included code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,17 @@
 get '/login'do
-  erb :'login/index'
+  erb :'sessions/index'
 end
 
 post '/login' do
-  @user = User.find_by(username: params[:user][:username])
+  @user = User.find_by(email: params[:user][:email])
   if @user && @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
-    redirect "users/#{@user.id}"
+    redirect "/users/#{@user.id}"
   else
     status 422
-    errors.add('login failed')
-    erb :'login/index'
+    # errors.add('The email or password is incorrect.')
+    @errors = ['The email or password is incorrect.']
+    erb :'sessions/index'
   end
 end
 
@@ -18,3 +19,4 @@ delete '/logout' do
   logout
   redirect '/'
 end
+
