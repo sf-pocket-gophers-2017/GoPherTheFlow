fb1128a1692fd2cff526416d1ba25f0650983b48
Garima & Jenna Ritten <pair+garimaj2108.jritten@devbootcamp.com>
2017-08-18 15:35:03 -0700

    Update question controller, corresponding views for new question/ show/ index page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,29 @@
+# Show all questions on the home page
 get '/questions' do
   @questions = Question.all
   erb :'questions/index'
 end
 
+# Fetch the form for posting a new question
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+# Post the form with question params
+post '/questions' do
+  @question = Question.new(params[:question])
+  if @question.save
+    redirect "/questions/#{@question.id}"
+  else
+    @errors = @question.errors.full_messages
+    erb :'questions/new'
+  end
+end
+
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+  @corr_answers = @question.answers
+
+  erb :'questions/show'
+end
 
